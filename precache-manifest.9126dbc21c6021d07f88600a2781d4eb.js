389509f6e4c9050a42d4fc33720fe7e2c3b1aa1d
Prankul Singh <prankul.singh@airtel.com>
2020-03-22 16:52:48 +0530

    Updates

---
+++ precache-manifest.9126dbc21c6021d07f88600a2781d4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdb506d5fc17c1212190217d2551141",
+    "url": "/quotes/public/index.html"
+  },
+  {
+    "revision": "d6eb674f5e691938a71f",
+    "url": "/quotes/public/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "679d32ed202b04ee4c27",
+    "url": "/quotes/public/static/js/2.ac7547ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes/public/static/js/2.ac7547ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6eb674f5e691938a71f",
+    "url": "/quotes/public/static/js/main.e308211f.chunk.js"
+  },
+  {
+    "revision": "8cc049f555822d2048da",
+    "url": "/quotes/public/static/js/runtime-main.504b6bbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quotes/public/static/media/logo.5d5d9eef.svg"
+  }
+]);
